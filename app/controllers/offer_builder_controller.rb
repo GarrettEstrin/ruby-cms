2b1707c9f899444ce10f054854986e9d8ee7c384
Garrett Estrin <garrett.estrin@gmail.com>
2017-11-27 23:21:48 -0800

    offers added with default properties

--- app/controllers/offer_builder_controller.rb
+++ app/controllers/offer_builder_controller.rb
@@ -1,21 +1,39 @@
 class OfferBuilderController < ApplicationController
     def index
-        $offers = JSON.parse $redis.GET('offers')
-        $new_offer = String.new
-        $offer_details = Hash.new
-        $offers.each do |key, value|
-            $offer_details[key] = JSON.parse $redis.GET('offer:' + key)
+        if $redis.GET('offers');
+            $offers = JSON.parse $redis.GET('offers')
+            $offer_details = Hash.new
+            $offers.each do |key, value|
+                $offer_details[key] = JSON.parse $redis.GET('offer:' + key)
+            end
         end
-        # $redis.SET('offers', {'att'=> '1'}.to_json)
+        $new_offer = String.new
     end
 
-    def add_offer 
-        $offers = JSON.parse $redis.GET('offers')
-        # $offer_details = JSON.parse $redis.SCAN(0, 'MATCH', 'offer:*')
+    def add_offer
+        # check if any offers exist
+        if $redis.GET('offers')
+            $offers = JSON.parse $redis.GET('offers')
+        else
+            $offers = Hash.new
+        end
+        # check if property tracking exists
+        if $redis.GET('properties')
+            $properties = JSON.parse $redis.GET('properties')
+        else
+            $properties = Hash.new
+            $properties["number"] = "888-888-8888"
+            $properties["name"] = ""
+            $redis.set("properties", $properties.to_json)
+        end
+        # set offer
         $offers[params[:offer_name]] = ""
         $redis.set('offers', $offers.to_json)
-        $offer = {"number" => "888-888-888"}
-        $redis.set('offer:' + params[:offer_name], $offer.to_json)
+        # set properties
+        $properties["name"] = params[:offer_name]
+        $redis.set('offer:' + params[:offer_name], $properties.to_json)
         redirect_to offerbuilder_path
     end 
+
+    
 end
